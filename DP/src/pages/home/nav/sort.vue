ab5b31371d1da894871da96f2f5996e4c4cd6fb4
qiqiOu <571283404@qq.com>
2019-04-24 22:14:15 +0800

    20190424

--- DP/src/pages/home/nav/sort.vue
+++ DP/src/pages/home/nav/sort.vue
@@ -1,13 +1,93 @@
 <template>
-    <div>sort</div>
+    <div>
+      <div class="sorts" v-for="(sort,index) in sorts">
+          <div class="title">{{sort.name}}</div>
+          <div class="lists" v-for="list in sort.lists">
+            <div class="list">
+              <img :src="list.pic" class="pic"/>
+              {{list.name}}
+            </div>
+          </div>
+      </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "sort"
+        name: "sort",
+        data(){
+          return {
+            sorts:[
+              {'name':'题材',
+                'lists':
+                  [
+                    {'name':'人像','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'动物','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'纪实','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'美女','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'静物','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'风光','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'旅行','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'建筑','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'街拍','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'}
+                  ]
+              },
+              {'name':'风格',
+                'lists':
+                  [
+                    {'name':'创意','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'后期','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'微距','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'黑白','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'色彩','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'日系','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'情绪','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'小清新','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'极简','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'}
+                  ]
+              },
+              {'name':'器材',
+                'lists':
+                  [
+                    {'name':'手机','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'广角','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'佳能','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'胶片','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'尼康','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'富士','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'索尼','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'35mm','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'},
+                    {'name':'50mm','pic':'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1343015815,2335192405&fm=26&gp=0.jpg'}
+                  ]
+              }
+            ]
+          }
+        }
     }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../../common/stylus/mixins.styl"
+  .sorts
+    width 90%
+    margin-left 5%
+    .title
+      border-left 5px solid $red
+      height 25px
+      padding-left 10px
+      line-height 25px
+      margin 20px 0px
+      color #999999
+    .lists
+      display inline-block
+      width 31%
+      margin 1px 1%
+      .list
+        background lightgray
+        width 100%
+        text-align center
+        .pic
+          width 100%
+          height auto
 
 </style>
--- DP/src/pages/home/nav/subscript_detail_movie.vue
+++ DP/src/pages/home/nav/subscript_detail_movie.vue
@@ -1,108 +1,62 @@
 <template>
-  <div>
-    <div class="background" >
-      <div class="top">
-        <i class="iconfont iconclose"></i>
-        <img src="../../../assets/imgs/1.jpeg" class="headpic">
-        <span class="username">用户名</span>
-        <button class="care">+关注</button>
-      </div>
-      <div class="videosty">
-        <video-player class="video-player vjs-custom-skin" ref="videoPlayer"
-                      :playsinline="true"
-                      :options="playerOptions" @click="isPlay">
-        </video-player>
-        <div style="height: 100%;width: 100%;background: crimson"></div>
-      </div>
+  <div class="background"  @click="isPlay">
+    <i class="iconfont iconreturn"></i>
+    <video :src="src" preload="auto"
+           loop="loop"
+           autoplay="autoplay"
+           controls="controls"
+           width="100%"
+           id="video"></video>
+    <div class="detail">
+      <button class="btn" v-for="mark in marks"># {{mark}} ></button>
+      <div class="username">@摄影师 {{name}}</div>
+      <div class="word">{{words}}</div>
     </div>
-    <!--<ul class="bottom">-->
-        <!--&lt;!&ndash;<li>&ndash;&gt;-->
-          <!--&lt;!&ndash;<div class="num">222</div>&ndash;&gt;-->
-          <!--&lt;!&ndash;<i class="iconfont iconxihuan_un"></i><div class="describe">喜欢</div>&ndash;&gt;-->
-        <!--&lt;!&ndash;</li>&ndash;&gt;-->
-        <!--&lt;!&ndash;<li>&ndash;&gt;-->
-          <!--&lt;!&ndash;<div class="num">222</div><i class="iconfont iconliuyan"></i><div class="describe">评论</div>&ndash;&gt;-->
-        <!--&lt;!&ndash;</li>&ndash;&gt;-->
-        <!--&lt;!&ndash;<li><i class="iconfont iconzhuanfa"></i><div class="describe">转发</div></li>&ndash;&gt;-->
-        <!--&lt;!&ndash;<li>&ndash;&gt;-->
-          <!--&lt;!&ndash;<div class="num">222</div>&ndash;&gt;-->
-          <!--&lt;!&ndash;<i class="iconfont iconshoucang_un"></i><div class="describe">收藏</div>&ndash;&gt;-->
-        <!--&lt;!&ndash;</li>&ndash;&gt;-->
-        <!--&lt;!&ndash;<li><i class="iconfont iconnext"></i><div class="describe">下一组</div></li>&ndash;&gt;-->
-        <!--&lt;!&ndash;<li><i class="iconfont iconxiazai"></i><div class="describe">下载</div></li>&ndash;&gt;-->
-    <!--</ul>-->
+    <div class="videosty cover">
+    </div>
+    <ul class="right">
+         <li>
+           <img src="../../../assets/imgs/add.png" class="add"></img>
+           <img  src="../../../assets/imgs/1.jpeg" class="headpic"/>
+         </li>
+        <li>
+          <i class="iconfont iconxihuan"></i><div class="describe">21</div>
+        </li>
+        <li>
+          <i class="iconfont iconxiaoxi"></i><div class="describe">31</div>
+        </li>
+        <li><i class="iconfont iconxiazai1"></i><div class="describe">45</div></li>
+        <li>
+          <img src="../../../assets/imgs/music.png" class="music"/>
+        </li>
+      </ul>
+
   </div>
 </template>
 
 <script>
   import { videoPlayer } from 'vue-video-player'
+  import Slider from "vue-concise-slider/src/components/slider";
 
   export default {
-    props: {
-      videoUrl: String,
-      state: Boolean
-    },
     data () {
       return {
-        play:true,
-        // videoUrl:'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
-        playerOptions: {
-        // playbackRates: [0.7, 1.0, 1.5, 2.0], //播放速度
-        autoplay: true, // 如果true,浏览器准备好时开始回放。
-        muted: false, // 默认情况下将会消除任何音频。
-        loop: true, // 导致视频一结束就重新开始。
-        preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
-        language: 'zh-CN',
-        aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
-        fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
-        sources: [{
-          type: 'video/mp4',
-          // src: this.videoUrl// 你的m3u8地址（必填）
-          src:'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4'
-        }],
-        // poster: 'https://surmon-china.github.io/vue-quill-editor/static/images/surmon-3.jpg', // 你的封面地址
-         width: document.documentElement.clientWidth,
-         notSupportedMessage: '此视频暂无法播放，请稍后再试',// 允许覆盖Video.js无法播放媒体源时显示的默认信息。
-         controlBar: {
-           timeDivider: true,
-           durationDisplay: true,
-           remainingTimeDisplay: false,
-           fullscreenToggle: true  //全屏按钮
-         }
-        }
+        src:'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
+        marks:['摄影'],
+        name:'用户名',
+        words:'我有好多小伙伴，小熊、小提琴、小卡车'
       }
     },
-    watch: {
-      //更改视频源 videoUrl从弹出框组件传值
-      videoUrl: function (val) {
-        if (val !== '') {
-          this.$refs.videoPlayer.player.src(val)
-        }
-      },
-      //弹出框关闭后暂停 否则一直在播放 state从弹出框组件传值
-      state: function (val) {
-        if (val) {
-          this.$refs.videoPlayer.player.pause()
-        }
-      }
-    },
-    components: {videoPlayer},
+    components: {Slider, videoPlayer},
     methods: {
       isPlay(){
-        if(this.play){
-          this.$refs.videoPlayer.player.pause();
-          this.play = false;
+        let video = document.getElementById('video');
+        console.log(video.paused);
+        if(video.paused) {
+          video.play();
+        } else {
+          video.pause();
         }
-        else{
-          this.$refs.videoPlayer.player.play();
-          this.play = true;
-        }
-        console(this.play);
-      }
-    },
-    computed: {
-      player () {
-        return this.$refs.videoPlayer.player
       }
     }
   }
@@ -113,59 +67,84 @@
   @import '../../../../node_modules/vue-video-player/src/custom-theme.css'
   @import "../../../common/stylus/mixins.styl"
   .background
-    background-color #222222
+    background-color #000000
     width 100%
     height 100%
     position absolute
-    .vjs-control-bar
-      display none
-    .videosty
-      margin-top 55%
-    .video-js .vjs-big-play-button
-      border none
-      background rgba(0,0,0,0)
-    .vjs-icon-placeholder::before
-      font-size: 70px;
-      opacity: 0.3;
-    .top
+    display flex
+    justify-items center
+    .iconreturn
       position fixed
       top 0
       width 90%
-      height 40px
+      color #eeeeee
+      font-size 25px
       line-height 40px
       padding 5px 5%
-      background rgba(0,0,0,0.2)
-      .iconclose
-        color white
-        font-size 20px
-        float left
-      .headpic
-        width 30px
-        height 30px
-        border-radius 50%
-        margin-left 10%
-        float left
-        margin-top 5px
-      .username
-        color #eeeeee
+    .detail
+      position absolute
+      bottom 20%
+      padding-left 5%
+      width 50%
+      color #eeeeee
+      .btn
+        border 1px solid #eeeeee
+        padding 2px 5px
         font-size 13px
-        margin-left 5%
-      .care
-        background-color $red
-        border none
         color #eeeeee
-        font-size 12px
-        margin-left 2%
-        padding 5px 15px
-        border-radius 10px
-    .bottom
-      position fixed
-      bottom 0px
-      width 90%
-      height auto
-      padding 20px 5%
-      background rgba(0,0,0,1)
+        border-radius 5px
+        background none
+        margin-right 10px
+        margin-bottom 10px
+      .username
+        font-size 15px
+        margin-bottom 10px
+      .word
+        font-size 15px
+    .right
+      position absolute
+      bottom 5%
+      right 5%
       display table-cell
       text-align center
-      z-index 2
+      color #ffffff
+      z-index 999
+      .iconfont
+        font-size 38px
+        color #ffffff
+      .headpic,.music
+        width 40px
+        height 40px
+        border-radius 50%
+        margin-bottom 30px
+        margin-top 5px
+      @keyframes changDeg
+        0%{transform: rotate(0deg); }
+        100%{transform: rotate(360deg);}
+      .music
+        animation changDeg 2s linear 0.2s infinite
+      .add
+        position absolute
+        right -5%
+        top 5%
+        color $red
+        border-radius 50%
+        width 25px
+        height 25px
+      .describe
+        margin-top 5px
+        margin-bottom 15px
+        font-size 15px
+  video::-webkit-media-controls-play-button,
+  video::-webkit-media-controls-mute-button,
+  video::-webkit-media-controls-volume-slider,
+  video::-webkit-media-controls-fullscreen-button,
+  video::-webkit-media-controls-current-time-display,
+  video::-webkit-media-controls-time-remaining-display
+    display none
+  video::-webkit-media-controls-timeline
+    width 100%
+    position absolute
+    margin 0
+    padding 0
 </style>
